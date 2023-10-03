2ecc49142bb1e7d923b6ae065bf763f2c6699b8f
Emanuele Urso <ursomanuele@gmail.com>
2023-10-04 01:15:52 +0200

    build_kernel.sh adapt to source

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 export ARCH=arm
-export PATH=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:$PATH
+export CROSS_COMPILE=/home/baelish/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 
 mkdir out
 
-make -C $(pwd) O=out CROSS_COMPILE=arm-linux-androideabi- j4primelte_sea_open_defconfig
-make -j64 -C $(pwd) O=out CROSS_COMPILE=arm-linux-androideabi-
+make -C $(pwd) O=out  j4primelte_defconfig
+make -j16 -C $(pwd) O=out CROSS_COMPILE=arm-linux-androideabi-
  
 cp out/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
